29016abf5fdf4c037fb63ebf3706f6ebeceea349
GreyStinger <greystinger@gmail.com>
2022-09-01 12:32:33 +0200

    config_updates

--- lib/config/example_config.h
+++ lib/config/example_config.h
@@ -1,15 +1,19 @@
 #ifndef _STATION_CONFIG
 #define _STATION_CONFIG
 
+// Hostname and MDNS instance name for local machines
 #define MDNS_INSTANCE "Example MDNS Instance"
 #define HOSTNAME "Example Local Hostname"
 
+// Local wifi configurations
 #define ESP_WIFI_SSID "Example SSID"
 #define ESP_WIFI_PASSWORD "Example Password"
 
+// Idk
 #define N_GPIOS_IN 1
 #define N_GPIOS_OUT 2
 
+// heh
 int GPIOS_IN[N_GPIOS_IN];
 int GPIOS_OUT[N_GPIOS_OUT];
 
